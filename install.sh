c2eccd57d21f6959dc6cbed9e221661c5fb29352
Damglador <vse.stopchanskyi@gmail.com>
2025-11-06 21:58:53 +0100

    Rename script to resize-media and require confirmation before overwriting

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
 basedir="$(dirname "$0")"
-install -Dm755 "$basedir/resize" \
-    "${XDG_BIN_HOME:-$HOME/.local/bin}/resize"
+if [ -f "${XDG_BIN_HOME:-$HOME/.local/bin}/resize-media" ] && [ "$1" != "-f" ]; then
+    cat << EOF
+File «${XDG_BIN_HOME:-$HOME/.local/bin}/resize-media» already exists.
+EOF
+    while true; do
+        read -rp "Replace the file? [y/N] " input
+        [ -z "$input" ] && input=n
+        [ "${input,,}" == n ] && break
+        [ "${input,,}" == y ] && \
+            install -Dm755 "$basedir/resize-media" \
+                "${XDG_BIN_HOME:-$HOME/.local/bin}/resize-media" && break
+    done
+else
+    install -Dm755 "$basedir/resize-media" \
+        "${XDG_BIN_HOME:-$HOME/.local/bin}/resize-media"
+fi
 install -Dm755 "$basedir/compress-media.desktop" \
     "${XDG_DATA_HOME:-$HOME/.local/share}/kio/servicemenus/compress-media.desktop"
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-rm "${XDG_BIN_HOME:-$HOME/.local/bin}/resize"
+rm "${XDG_BIN_HOME:-$HOME/.local/bin}/resize-media"
 rm "${XDG_DATA_HOME:-$HOME/.local/share}/kio/servicemenus/compress-media.desktop"
